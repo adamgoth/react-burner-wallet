c2872778c8da7c93d96ceebaffc00599bc062a14
Adam Goth <adamgoth@gmail.com>
2019-02-16 15:39:04 -0700

    Rename wallet component to avoid confusion

--- src/BurnerWallet.js
+++ src/BurnerWallet.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Sidebar from "react-sidebar";
-import Wallet from "./BurnerWallet/BurnerWallet";
+import Wallet from "./WalletSrc/WalletSrc";
 import { ReactComponent as Logo } from "./burner-wallet.svg";
 
 export default class App extends Component {
