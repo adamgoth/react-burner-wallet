799ec95da3b2180d3a5b35ee34aa7bd83b458b27
Adam Goth <adamgoth@gmail.com>
2019-02-17 08:32:15 -0700

    Updates

---
+++ precache-manifest.3e3cafb82e7ddb18673f1e24b5ca76f9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/react-burner-wallet/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "fa8dc606914569b0ead3",
+    "url": "/react-burner-wallet/static/css/main.20fff9b5.chunk.css"
+  },
+  {
+    "revision": "492dc905589f19ab4477",
+    "url": "/react-burner-wallet/static/js/runtime~main.492dc905.js"
+  },
+  {
+    "revision": "136ef8c5c7efcd7e58d35b70a1dae635",
+    "url": "/react-burner-wallet/static/media/burner-wallet.136ef8c5.svg"
+  },
+  {
+    "revision": "4c738a5638795c7cfba8",
+    "url": "/react-burner-wallet/static/js/2.4c738a56.chunk.js"
+  },
+  {
+    "revision": "5b15a8c6575231a6683cc8982faab6eb",
+    "url": "/react-burner-wallet/static/media/dai.5b15a8c6.jpg"
+  },
+  {
+    "revision": "17fb2141e1acbb4caf3004515da0a755",
+    "url": "/react-burner-wallet/static/media/burnerloader.17fb2141.gif"
+  },
+  {
+    "revision": "f636a29165bc40ac68324d3142aba740",
+    "url": "/react-burner-wallet/static/media/burnerwallet.f636a291.png"
+  },
+  {
+    "revision": "58196683b28e9930b35c993d1ae20ff6",
+    "url": "/react-burner-wallet/static/media/customRPCHint.58196683.png"
+  },
+  {
+    "revision": "e2983bb01d81943642bf94b46743ce29",
+    "url": "/react-burner-wallet/static/media/bufficorn.e2983bb0.png"
+  },
+  {
+    "revision": "f0ff9cc44e4e0a6b7551647a6cc99134",
+    "url": "/react-burner-wallet/static/media/cypherpunk.f0ff9cc4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-burner-wallet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/react-burner-wallet/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/react-burner-wallet/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/react-burner-wallet/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "fa8dc606914569b0ead3",
+    "url": "/react-burner-wallet/static/js/main.fa8dc606.chunk.js"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/react-burner-wallet/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/react-burner-wallet/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/react-burner-wallet/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/react-burner-wallet/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/react-burner-wallet/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/react-burner-wallet/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/react-burner-wallet/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/react-burner-wallet/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/react-burner-wallet/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/react-burner-wallet/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/react-burner-wallet/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "76e57817ad4d57d2c50b9f01d16c0cb4",
+    "url": "/react-burner-wallet/static/media/localeth.76e57817.png"
+  },
+  {
+    "revision": "4c738a5638795c7cfba8",
+    "url": "/react-burner-wallet/static/css/2.c09ad4f9.chunk.css"
+  },
+  {
+    "revision": "f57784ee62106daf1d2ee9eeef4924ab",
+    "url": "/react-burner-wallet/index.html"
+  }
+];
